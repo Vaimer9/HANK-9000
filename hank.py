001d54f0cd4e670efaaa21ce7d540585b1f9c3b6
Vaimer9 <vaim3r9@gmail.com>
2021-07-13 13:35:28 +0530

    Refactored all of the code

--- hank.py
+++ hank.py
@@ -24,39 +24,31 @@ def greet(string, repeat):
 
 
 @cli.command()
-@click.option('--first', type=click.INT, default=None, prompt="First Number" ,help="First Number")
-@click.option('--second',type=click.INT,default=None, prompt="Second Number" ,help="Second Number")
-@click.option("--operation",type=click.STRING,  default="None")
+@click.argument('first', type=click.INT,)
+@click.argument('second',type=click.INT)
+@click.argument("operation",type=click.STRING)
 def math(first, second, operation):
     """ This Command can do 2 number maths """
-    first = int(first)
-    
     result = None
-    second = int(second)
-    if first and second == None:
-        click.echo(message="Please enter at least two numbers")
+    if operation == "add":
+        result = first + second
+    elif operation == "sub":
+        result = first - second
+    elif operation == "mul":
+        result = first * second
+    elif operation == "div":
+        result = first / second
     else:
-        if operation == "+":
-            result = first + second
-        elif operation == "-":
-            result = first - second
-        elif operation == "m":
-            result = first * second
-        elif operation == "/":
-            result = first / second
-        elif operation == "None":
-            click.echo(message="I am pretty sure this error will never be displayed :/")
-        else:
-            click.echo(message="Please enter the correct type of data.")
-        click.echo(result)
+        click.echo(message="Please enter the correct type of data.")
+    click.echo(result)
+        
 
 
 
 @cli.command()
-@click.option('--query', type=click.STRING, default=None, prompt="What is your search query", help="The string to be searched")
+@click.argument('query', type=click.STRING)
 def google(query):
     """Google stuff on the internet"""
-    
     for x in search(query, tld="co.in", num=10, stop=10, pause=2):
         click.echo(message=x)
 
@@ -74,7 +66,7 @@ def datetime():
 
 @cli.command()
 @click.argument('file', type=click.STRING, default=None)
-@click.option('--msg', type=click.STRING, default=None)
+@click.argument('msg', type=click.STRING, default=None, required=False)
 def create(file, msg):
     """Makes a files and writes stuff down in it"""
     f = None
